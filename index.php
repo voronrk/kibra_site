e2e6af76d447ea6746dbaf27fad358f8dd28e527
voronrk <voron_76@mail.ru>
2022-10-13 02:07:37 +0300

    Dirty layout

--- index.php
+++ index.php
@@ -4,14 +4,83 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/bulma.min.css">
     <link rel="stylesheet" href="./styles/styles.css">
-    <!-- <link rel="stylesheet" href="./styles/bulma.min.css"> -->
 
     <title>КИБРА - разработка приложений под Битрикс24</title>
 </head>
 
 <body>
-    <header class="header">
+    <div class="navbar is-main header">
+        <div class="container">
+            <div class="navbar-brand">
+                <a class="navbar-item" href="https://kibra24.ru">
+                    <div class="logo">
+                        <div class="logo-header-title">
+                            КИБРА24
+                        </div>
+                        <div class="logo-header-subtitle">
+                            Разработка приложений для Битрикс24
+                        </div>
+                    </div>
+                </a>
+            </div>
+
+            <div class="navbar-end">
+                <div class="navbar-item">
+                    <a href="https://t.me/voron_76"><img src="img/t_logo.svg" alt="Telegram"></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="main-wrapper">
+        <div class="container">
+            <h1 class="title is-1 has-text-weight-bold has-text-white has-text-centered">
+                Разработка приложений для Битрикс24
+            </h1>
+            <h2 class="subtitle is-5 has-text-weight-bold has-text-white has-text-centered">
+                Приложения, активити, роботы, бизнес-процессы, интеграции
+            </h2>
+            <div class="columns is-centered">
+                <div class="column is-half">
+                    <form action="send.php" class="form has-background-white p-6">
+                        <h2 class="subtitle is-3 has-text-weight-bold has-text-centered">Отправить заявку</h2>
+                        <input type="text" class="input my-4 is-medium" placeholder="Имя"></input>
+                        <input type="email" class="input my-4 is-medium" placeholder="E-mail"></input>
+                        <textarea class="textarea my-4 is-medium" placeholder="Заявка" rows="3"></textarea>
+                        <input type="submit" class="button is-link is-medium is-fullwidth"></input>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer is-main">
+        <div class="container">
+            <div class="columns">
+                <div class="column">
+                    <div class="logo">
+                        <div class="logo-footer-title">
+                            КИБРА24
+                        </div>
+                        <div class="logo-footer-subtitle">
+                            Разработка приложений для Битрикс24
+                        </div>
+                    </div>
+                </div>
+                <div class="column">
+                    <button class="button is-white">Оплатить заказ</button>    
+                </div>
+                <div class="column">
+                    <ul class="footer-menu">
+                        <li>Юридическая информация</li>
+                        <li>Оплата</li>
+                        <li>Порядок выполнения заказа</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- <header class="header">
         <div class="container dir-row">
             <div class="logo">
                 <div class="logo-header-title">
@@ -66,6 +135,6 @@
                 <li>Порядок выполнения заказа</li>
             </ul>
         </div>
-    </footer>
+    </footer> -->
 </body>
 </html>
